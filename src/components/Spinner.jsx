efd8655da7f75d2833c3b4e05e11013b9a167800
Al Amin <alaminkhan10009@gmail.com>
2025-05-29 22:30:02 +0600

    added orange spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,7 @@ export default function Spinner(){
     return (
         <>
         <div className="flex justify-center items-center min-h-screen">
-      <div className="animate-spin  border-4 border-blue-400 w-16 h-16 border-dashed rounded-full">
+      <div className="animate-spin  border-4 border-orange-400 w-16 h-16 border-dashed rounded-full">
 
       </div>
         </div>
